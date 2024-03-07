497d52c86cb38d4df9f0865401c6c21f57acdbcb
Lucas Bach <lborgesbach@gmail.com>
2024-03-07 13:42:12 -0300

    def sacar

--- operation/oper_system.py
+++ operation/oper_system.py
@@ -4,3 +4,13 @@ saldo = 500
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
+def sacar():
+    global saldo
+
+    while True:
+        valor_input = input('Digite o valor: ')
+        if valor_input.isdigit():
+            Saldo_disp = float(valor_input)
+            break
+        else:
+            print('Insira apenas números.')
